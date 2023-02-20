94e0ef2edeec8642f7d6bc1f28a7265a37aba2a3
Aakash Raj <aakashraj416@gmail.com>
2023-02-20 09:29:42 +0530

    Search Insert Position

---
+++ 35. Search Insert Position.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int ans = nums.size();
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] >= target)
+            {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
