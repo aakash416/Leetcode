13975e3f67498fdccfc96062d814dedfa7ba3984
Aakash Raj <aakashraj416@gmail.com>
2023-03-20 12:18:16 +0530

    Can Place Flowers

--- 23. Merge k Sorted Lists.cpp
+++ 23. Merge k Sorted Lists.cpp
@@ -8,7 +8,6 @@ struct ListNode
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-
 };
 class Solution
 {
